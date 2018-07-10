fc9ea66f53deceba723c9013fcc886f71849b6ed
chengkai <cheng_kai@suixingpay.com>
2018-07-10 11:36:29 +0800

    提交ios library

---
+++ reactnativeLibrary/reactnativeLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  reactnativeLibrary
+//
+//  Created by chengkai on 2018/7/10.
+//  Copyright © 2018年 suixingpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ reactnativeLibrary/reactnativeLibrary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  reactnativeLibrary
+//
+//  Created by chengkai on 2018/7/10.
+//  Copyright © 2018年 suixingpay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
